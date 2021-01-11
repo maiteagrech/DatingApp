d9c2eeb2ee9018d0bf59940fad61ff775231e539
Maite <maite.agrech@outlook.com>
2021-01-11 10:17:43 -0300

    New members location

--- SPA/src/app/content/content.module.ts
+++ SPA/src/app/content/content.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { MaterialModule } from "../material/material.module";
 import { ContentRoutes } from "./content.routing";
-import { ListsComponent } from "./lists/lists.component";
-import { MatchesComponent } from "./matches/matches.component";
-import { MessagesComponent } from "./messages/messages.component";
-import { UsersComponent } from "./users/users.component";
+import { ListsComponent } from "./members/lists/lists.component";
+import { MatchesComponent } from "./members/matches/matches.component";
+import { MessagesComponent } from "./members/messages/messages.component";
 
 @NgModule({
-    declarations: [UsersComponent, MatchesComponent, ListsComponent, MessagesComponent],
-    imports: [RouterModule.forChild(ContentRoutes), MaterialModule, CommonModule],
+  declarations: [MatchesComponent, ListsComponent, MessagesComponent],
+  imports: [RouterModule.forChild(ContentRoutes), MaterialModule, CommonModule],
 })
 export class ContentModule {}
--- SPA/src/app/content/content.routing.ts
+++ SPA/src/app/content/content.routing.ts
@@ -1,8 +1,8 @@
 import { Routes } from "@angular/router";
 import { AuthGuard } from "../auth.guard";
-import { ListsComponent } from "./lists/lists.component";
-import { MatchesComponent } from "./matches/matches.component";
-import { MessagesComponent } from "./messages/messages.component";
+import { ListsComponent } from "./members/lists/lists.component";
+import { MatchesComponent } from "./members/matches/matches.component";
+import { MessagesComponent } from "./members/messages/messages.component";
 
 export const ContentRoutes: Routes = [
   {
